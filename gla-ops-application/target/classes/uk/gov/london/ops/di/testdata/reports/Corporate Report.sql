496dfd023597f3e0a864e0f4e5fa3cfa002060c4
summani <summani@deloitte.co.uk>
2022-03-25 16:43:06 +0000

    Removed target folders

--- gla-ops-application/target/classes/uk/gov/london/ops/di/testdata/reports/Corporate Report.sql
+++
@@ -1,6 +0,0 @@
-select p.org_id as organisation_id, p.managing_organisation_id, p.programme_id, pg.name, o.team_id as team_id, psl.label_name as label from project p
-inner join organisation o on p.org_id = o.id
-inner join programme pg on p.programme_id = pg.id
-inner join label l on p.id = l.project_id
-inner join pre_set_label psl on l.pre_set_label_id = psl.id
-where p.programme_id in (:programme_ids) and l.pre_set_label_id in (:label_ids)
